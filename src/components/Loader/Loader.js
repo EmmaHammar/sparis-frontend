8fc033bd94e7448c650e12b57e7c44fec5a3d3fa
Sara Sedaghat <75505464+Sarasedaghat8960@users.noreply.github.com>
2021-11-03 21:46:29 +0100

    Add css to Login and loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Loader.module.scss'
 
 export default function Loader() {
     return (
-        <div>
-            
+        <div className={styles.wrapper}>
+           <i>Loader</i> 
         </div>
     )
 }
