9993a6a7dd2526ce630685fd674aa36ec6c08d8e
Axel Pahne <random@wwho.com>
2021-11-05 10:17:43 +0100

    Resolved merge conflict

--- src/components/SavingGoal/ShowSavingGoal/ShowSavingGoal.js
+++ src/components/SavingGoal/ShowSavingGoal/ShowSavingGoal.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './ShowSavingGoal.module.scss'
 
-export default function ShowSavingGoal({clickedChildObj}) {
-    
+export default function ShowSavingGoal({ clickedChildObj }) {
+
     return (
         <div className={styles.wrapper} id="showSavingGoalContainer">
             <i>ShowSavingGoal</i>
+
+            {/*Fix  error */}
             <h3>{clickedChildObj.goalTitle}</h3>
             <img src={clickedChildObj.goalPic} alt={clickedChildObj.goalTitle} id="goalPic"></img>
             <p>Pris: {clickedChildObj.goalAmount} kr</p>
