80d9fb64e9dcfa4bfc5f0588a7a4f0a39ff07de8
EmmaHammar <emma.s.hammar@gmail.com>
2021-11-08 10:56:15 +0100

    fix error w showing savingGoal or no depending on goalAmount, clickedChild and accountId login

--- src/pages/Adults.js
+++ src/pages/Adults.js
@@ -6,28 +6,34 @@ import SavingGoalAdults from '../containers/Adults/SavingGoalAdults'
 
 export default function Adults({dbAccount, dbParent, dbChildren}) {
 
-    //NOTE expect accountId from login ok? 
-    const [loginAccountId, setLoginAccountId] = useState(1);
-
+    //NOTE expect accountId from login ok? Get from localStorage?
+    const [loginAccountId, setLoginAccountId] = useState(1); 
     const [clickedChildObj, setClickedChildObj] = useState({});
-
     const [showChildProfile, setShowChildProfile] = useState(false);
-
+    const [savingGoalExists, setSavingGoalExists] = useState(false);
 
     const handleClick = (evt) => {
-        // setClickedChildId(evt.target.id);
-        // setClickedChildUserName(evt.target.value);
+        // reset savingGoalExists:
+        if (savingGoalExists === true) {
+            setSavingGoalExists(false);
+        }
+        if (savingGoalExists === false) {
+            setSavingGoalExists(true);
+        } 
+
         setShowChildProfile(true);
 
-
         //Save clickedChildInfo:
-        //NOTE if === => not working :( 
         const isChildId = (child) => {
             return child.childId == evt.target.id;
         };
         setClickedChildObj(dbChildren.children.find(isChildId));
 
-        console.log("klick child");
+        if (dbChildren.children.find(isChildId).goalAmount !== 0) {
+            setSavingGoalExists(true);
+        } else {
+            setSavingGoalExists(false);
+        }
     };
     
     return (
@@ -44,13 +50,10 @@ export default function Adults({dbAccount, dbParent, dbChildren}) {
             {showChildProfile ? 
                <>
                 <h2>{clickedChildObj.userName}</h2>
-                <SavingGoalAdults clickedChildObj={clickedChildObj}/>
+                <SavingGoalAdults clickedChildObj={clickedChildObj} savingGoalExists={savingGoalExists}/>
                 <TotalSavingsAdults clickedChildObj={clickedChildObj}/>
                </>
             : ""}
-
-
-
            
             <NavFooterAdults />
         </div>
