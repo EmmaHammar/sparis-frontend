f967b42639d3dc0515d31e04c3fba9a9815066f4
EmmaHammar <emma.s.hammar@gmail.com>
2021-11-15 13:43:13 +0100

    fix bug printing savingGoal if it exists

--- src/containers/general/savingGoal.js
+++ src/containers/general/savingGoal.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 
 import { NoSavingGoal, AddSavingGoal, ShowSavingGoal, DeleteSavingGoal, ActivateGoalComplete, GoalComplete } from '../../components/SavingGoal/index';
 
-export default function SavingGoal(props) {
-    const [savingGoalExists, setSavingGoalExists] = useState(props.savingGoalExists)
-    const [showChild, setShowChild] = useState(props.showChild);
-   
+export default function SavingGoal({ savingGoalExists, showChild}) {
+// export default function SavingGoal(props) {
+    // const [savingGoalExists, setSavingGoalExists] = useState(props.savingGoalExists);
+    // const [showChild, setShowChild] = useState(props.showChild);
 
     //cb savingGoalExists true
     const changeSavingGoalExists = (newGoal) => {
-        setSavingGoalExists(true);
+        // setSavingGoalExists(true); 
 
         //add newGoal to showChild state
-        let updatedShowChild = {...showChild};
-        updatedShowChild = newGoal;
-        setShowChild(updatedShowChild);
-    }
+        // let updatedShowChild = {...showChild};
+        // updatedShowChild = newGoal;
+        // setShowChild(updatedShowChild); 
+    };
 
     return (
         <div style={{ border: "1px solid red" }} id="savingGoalContainer">
