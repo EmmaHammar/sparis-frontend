70477dd0e369ce84ca2734ad200dc8a740308f0f
EmmaHammar <emma.s.hammar@gmail.com>
2021-11-18 00:41:44 +0100

    add CSS to savingGoal, totalSavings and Login

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   return (
 
     <>
-    {isloading ? <Loader /> : 
+    {/* {isloading ? <Loader /> :  */}
     
     <div style={{ maxWidth: "375px", display:"flex", justifyContent: "center", padding: " 0.5rem 0rem", margin: "30px auto", boxSizing: "none"}}>
       {/* Add Loader later when testing finish */}
@@ -31,7 +31,6 @@ function App() {
           </Routes>
         </Router>
     </div>
-  }
     </>
   );
 }
